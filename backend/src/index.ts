16a863b6ae48516fc6ac56379cea2922c0b83a8b
healthlinkainetwork <healthlinkainetwork@gmail.com>
2025-12-08 17:35:52 -0500

    Wire ratings routes into backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,7 @@ import authRouter from "./routes/auth";
 import caregiversRouter from "./routes/caregivers";
 import jobsRouter from "./routes/jobs";
 import agenciesRouter from "./routes/agencies";
+import ratingsRouter from "./routes/ratings";
 
 config();
 
@@ -24,6 +25,7 @@ app.use("/auth", authRouter);
 app.use("/caregivers", caregiversRouter);
 app.use("/jobs", jobsRouter);
 app.use("/agencies", agenciesRouter);
+app.use("/ratings", ratingsRouter);
 
 app.get("/", (_req, res) => {
   res.json({ ok: true, message: "HealthLink backend API root" });
